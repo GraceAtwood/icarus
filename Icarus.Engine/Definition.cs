f5bbf3af18c0cb8284707d789e4e0299cc3d8edb
Daniel Atwood <dkatwood@amazon.com>
2018-12-23 03:45:15 -0700

    Initial commit

---
+++ Icarus.Engine/Definition.cs
@@ -0,0 +1,7 @@
+namespace Icarus.Engine
+{
+    public class Definition
+    {
+        
+    }
+}
---
+++ Icarus.Engine/Things/ThingDefinition.cs
@@ -0,0 +1,7 @@
+namespace Icarus.Engine.Things
+{
+    public class ThingDefinition
+    {
+        
+    }
+}
